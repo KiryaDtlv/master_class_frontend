4db7b41dd8b21f7ed469e2b4ed4e5e595b0532b6
k.diatlov <k.diatlov@agora.ru>
2025-09-04 11:39:51 +0700

    Поломанный чат

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -4,8 +4,6 @@ import { MessageInput } from './MessageInput';
 export const ChatWindow = ({ ws, messages, username }) => {
   return (
     <div className="chat-window">
-      <MessageList messages={messages} />
-      <MessageInput ws={ws} username={username} />
     </div>
   );
 };
